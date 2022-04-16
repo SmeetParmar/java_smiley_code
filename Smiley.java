303d4a702c623351ca087911f277b8dcd30a2136
Smeet Parmar <99462995+SmeetParmar@users.noreply.github.com>
2022-04-16 19:26:53 +0530

    Update Smiley.java

--- Smiley.java
+++ Smiley.java
@@ -5,13 +5,13 @@ public class Smiley extends Applet {
  
  public void paint(Graphics g) {
   
-  // g.setColor(Color.yellow);
+  //g.setColor(Color.yellow);
   g.drawOval(20,20,150,150);   // For face
-// g.setColor(Color.black);
+  //g.setColor(Color.black);
   g.fillOval(50,60,15,25);     // Left Eye 
   g.fillOval(120,60,15,25);    // Right Eye
- // int x[] = {95,85,106,95};
- // int y[] = {85,104,104,85};
+  //int x[] = {95,85,106,95};
+  //int y[] = {85,104,104,85};
   //g.drawPolygon(x, y, 4);      // Nose
   g.drawArc(55,95,78,50,0,-180);  // Smile
   g.drawLine(50,126,60,116);   // Smile arc1
